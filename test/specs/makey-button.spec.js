018eaa621612cf2ce5229872b0b72793955ad14a
Andrew Stewart <andrew@stwrt.ca>
2014-03-17 14:09:42 -0700

    Improve specs for MakeyButton driver

--- test/specs/makey-button.spec.js
+++ test/specs/makey-button.spec.js
@@ -1,13 +1,14 @@
 "use strict";
 
-var MakeyButton = source("makey-button");
+source("makey-button");
 
 describe("Cylon.Drivers.GPIO.MakeyButton", function() {
-  var driver = new MakeyButton({
+  var driver = new Cylon.Drivers.GPIO.MakeyButton({
     name: 'button',
     device: {
-      connection: 'connect',
-      pin: 13
+      connection: { digitalRead: spy() },
+      pin: 13,
+      emit: spy()
     }
   });
 
@@ -17,7 +18,11 @@ describe("Cylon.Drivers.GPIO.MakeyButton", function() {
     });
 
     it("sets @isPressed to false by default", function() {
-      expect(driver.isPressed).to.be["false"];
+      expect(driver.isPressed).to.be.eql(false);
+    });
+
+    it("sets @currentValue to 0 by default", function() {
+      expect(driver.currentValue).to.be.eql(0);
     });
 
     it("sets @data to an empty array by default", function() {
@@ -25,8 +30,91 @@ describe("Cylon.Drivers.GPIO.MakeyButton", function() {
     });
   });
 
-  it("provides an array of makey-button commands", function() {
-    expect(driver.commands()).to.be.eql(['isPressed']);
+  describe("#commands", function() {
+    var commands = driver.commands();
+    it("provides an array of makey-button commands", function() {
+      expect(commands).to.be.an('array');
+
+      for (var i = 0; i < commands.length; i++) {
+        expect(commands[i]).to.be.a('string');
+      }
+    });
+  });
+
+  describe("#start", function() {
+    before(function() {
+      this.clock = sinon.useFakeTimers();
+      driver.start(function() {});
+    });
+
+    after(function() {
+      this.clock.restore();
+    });
+
+    it("reads the value of the pin into @currentValue", function() {
+      expect(driver.connection.digitalRead).to.be.calledWith(13);
+    });
+
+    describe("button-checking loop", function() {
+      it("adds @currentValue to @data every 50ms", function() {
+        expect(driver.data).to.be.eql([]);
+        this.clock.tick(51);
+        expect(driver.data).to.be.eql([0]);
+      });
+
+      it("keeps the array at 5 items by shifting out the oldest values", function() {
+        driver.data = [];
+        this.clock.tick(251);
+        expect(driver.data).to.be.eql([0, 0, 0, 0, 0]);
+        driver.currentValue = 1;
+        this.clock.tick(151);
+        expect(driver.data).to.be.eql([0, 0, 1, 1, 1]);
+      });
+
+      describe("if @averageData is greater than 0.5", function() {
+        before(function() {
+          driver.isPressed = true;
+          driver.device.emit = spy();
+          stub(driver, 'averageData').returns(0.65);
+        });
+
+        after(function() {
+          driver.averageData.restore();
+        });
+
+        it("emits 'release' once", function() {
+          this.clock.tick(150);
+          expect(driver.device.emit).to.be.calledWith('release');
+          expect(driver.device.emit).to.be.calledOnce;
+        });
+
+        it("sets @isPressed to false", function() {
+          expect(driver.isPressed).to.be.eql(false);
+        });
+      });
+
+      describe("if @averageData is less than 0.5", function() {
+        before(function() {
+          driver.isPressed = false;
+          driver.device.emit = spy();
+          stub(driver, 'averageData').returns(0.45);
+        });
+
+        after(function() {
+          driver.averageData.restore();
+        });
+
+        it("emits 'push' once", function() {
+          this.clock.tick(150);
+          expect(driver.device.emit).to.be.calledWith('push');
+          expect(driver.device.emit).to.be.calledOnce;
+        });
+
+        it("sets @isPressed to true", function() {
+          expect(driver.isPressed).to.be.eql(true);
+        });
+      });
+    });
   });
 
   describe('#averateData', function() {
