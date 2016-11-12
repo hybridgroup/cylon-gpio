b1722fdca3ae736b79ce140796b64acf42d91a78
deadprogram <ron@hybridgroup.com>
2016-11-12 15:00:20 +0100

    Adds simple directional control for motors that can support it
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/racecar/racecar.js
@@ -0,0 +1,32 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: {
+    edison: "intel-iot"
+  },
+
+  devices: [
+    {name: "leftMotor", driver: "motor", pin: "5", directionPin: "4"},
+    {name: "rightMotor", driver: "motor", pin: "6", directionPin: "7"},
+  ],
+
+  work: function(my) {
+    my.leftMotor.forward(100);
+    my.rightMotor.forward(100);
+    after((5).seconds(), function() {
+      my.leftMotor.speed(25);
+    });
+    after((10).seconds(), function() {
+      my.leftMotor.speed(100);
+      my.rightMotor.speed(25);
+    });
+    after((20).seconds(), function() {
+      my.leftMotor.speed(100);
+      my.rightMotor.speed(100);
+    });
+    after((25).seconds(), function() {
+      my.leftMotor.turnOff();
+      my.rightMotor.turnOff();
+    });
+  }
+}).start();
